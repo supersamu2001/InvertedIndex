ec3b1c879e1d05439d0e9c539537d2de60890ad5
supersamu2001 <samuele2001marchi@gmail.com>
2025-05-17 19:56:46 +0200

    SCIAMN

--- invertedIndex/src/main/java/it/unipi/hadoop/InvertedIndex.java
+++ invertedIndex/src/main/java/it/unipi/hadoop/InvertedIndex.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
@@ -21,17 +22,23 @@ public class InvertedIndex
         Job job = Job.getInstance(conf, "inverted index");
         job.setJarByClass(InvertedIndex.class);
         job.setMapperClass(InvertedIndexMapper.class);
-        // job.setCombinerClass(InvertedIndexCombiner.class);
+        job.setCombinerClass(InvertedIndexCombiner.class);
         job.setReducerClass(InvertedIndexReducer.class);
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(CountPerFile.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        for (int i = 0; i < otherArgs.length - 1; ++i) {
+
+        job.setInputFormatClass(CombineTextInputFormat.class); // Combine many small files
+        CombineTextInputFormat.setMaxInputSplitSize(job, 134217728); // 128MB
+
+        /*
+        for (int i = 0; i < otherArgs.length - 1; ++i)
             FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-        }
-        FileOutputFormat.setOutputPath(job,
-                new Path(otherArgs[otherArgs.length - 1]));
+         */
+
+        CombineTextInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
--- invertedIndex/src/main/java/it/unipi/hadoop/InvertedIndexMapper.java
+++ invertedIndex/src/main/java/it/unipi/hadoop/InvertedIndexMapper.java
@@ -1,11 +1,15 @@
 package it.unipi.hadoop;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 /*
@@ -14,22 +18,63 @@ import java.util.StringTokenizer;
  */
 
 public class InvertedIndexMapper extends Mapper<Object, Text, Text, CountPerFile> {
-    // private final static IntWritable one = new IntWritable(1);
-    // private final Text token_key = new Text();
+    private final static IntWritable one = new IntWritable(1);
+    private final Text token_key = new Text();
+    private final List<String> stop_words = new ArrayList<>();
+
+    public void setup(Context context) {
+        try {
+            // Otteniamo la lista di stop words che andranno eliminate da ogni file, da parte dei relativi mapper.
+            String fileName = "stopwords.txt";
+            InputStream input = InvertedIndexMapper.class.getClassLoader().getResourceAsStream(fileName);
+            if (input == null)
+                throw new IllegalArgumentException("File not found: " + fileName);
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+                String linea;
+                while ((linea = reader.readLine()) != null) {
+                    if (!linea.trim().isEmpty()) {
+                        stop_words.add(linea.trim());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
-        // Ottieni lo split attuale, da cui puoi risalire al file
+        // Ottiene lo split attuale, da cui risalire al file
         FileSplit fileSplit = (FileSplit) context.getInputSplit();
         String fileName = fileSplit.getPath().getName();
 
-        // suddivide il file in token (parola singola)
-        final StringTokenizer itr = new StringTokenizer(value.toString());
+        // rimuove parole e caratteri insignificanti
+        String cleaned_words = preprocessing(value.toString());
 
-        while (itr.hasMoreTokens()) {
-            String word = itr.nextToken();
-            CountPerFile countPerFile = new CountPerFile(new Text(fileName), new IntWritable(1));
-            context.write(new Text(word), countPerFile);
+        // suddivide il file in token (parola singola)
+        final StringTokenizer itr = new StringTokenizer(cleaned_words);
+
+        while(itr.hasMoreTokens()) {
+            token_key.set(itr.nextToken());
+            CountPerFile countPerFile = new CountPerFile(new Text(fileName), one);
+            context.write(token_key, countPerFile);
         }
 
+    }
+
+    private String preprocessing(String text){
+        // Ottenere il testo tutto minuscolo
+        StringBuilder processed_words = new StringBuilder();
+        String textLowerCase = text.toLowerCase();
+        String[] tokens = textLowerCase.split("\\W+");                 // Divide il testo in singoli token
+        for(String token : tokens){
+            if(!token.isEmpty() && !stop_words.contains(token))
+                processed_words.append(token).append(" ");
+        }
+        return processed_words.toString()
+                .replaceAll("'s\\b", "")       // Rimuove genitivi sassoni
+                .replaceAll("[^a-z\\s]", " ")  // Rimuove caratteri non alfanumerici
+                .replaceAll("\\s+", " ")       // Riduce spazi multipli a uno singolo
+                .trim();                                      // Rimuove spazi in cima e in fondo
     }
 }
