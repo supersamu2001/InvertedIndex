41e1d5b6feef7fae80b2552e1e218d1650b84579
supersamu2001 <samuele2001marchi@gmail.com>
2025-06-06 18:49:10 +0200

    graphs

---
+++ graphs.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+
+'''
+# Inserisci le dimensioni dei dataset (valori comuni per entrambe le funzioni)
+x_values = [0.25, 94, 353, 706, 1100, 2200]
+
+# KB, MB1, MB2, MB3, GB1, GB2
+
+# Tempi di esecuzione per la Funzione 1
+y_hadoop_combiner = [13.5, 111, 181, 289, 571, 790]
+
+y_hadoop_inmapper = [13, 87, 168.5, 187, 464, 663]
+
+# Tempi di esecuzione per la Funzione 2
+y_nonParallel = [0.1, 0.5, 133, 202, 830, 1968]
+
+y_spark = [7, 34, 108, 210, 378, 606]
+
+plt.figure(figsize=(10, 6))
+
+# Tracciamo le due funzioni
+plt.plot(x_values, y_hadoop_combiner, marker='o', label='Hadoop with combiner function', color='blue')
+plt.plot(x_values, y_hadoop_inmapper, marker='*', label='Hadoop with in-mapper function', color='yellow')
+plt.plot(x_values, y_nonParallel, marker='s', label='Non-parallel function', color='green')
+plt.plot(x_values, y_spark, marker='^', label='Spark function', color='red')
+
+
+
+# Aggiungiamo etichette e titolo
+plt.xlabel("Dataset size (MB)")
+plt.ylabel("Elapsed time (s)")
+plt.title("Time comparison")
+plt.legend()
+plt.grid(True)
+
+# Mostra il grafico
+plt.tight_layout()
+plt.savefig("Hadoop-Spark-NonParallel (time).png", dpi=300)  # dpi=300 per alta qualità
+plt.show()
+'''
+# Inserisci le dimensioni dei dataset (valori comuni per entrambe le funzioni)
+x_values = [0.25, 94, 353, 706, 1100, 2200]
+
+# Tempi di esecuzione per la Funzione 1
+y_hadoop_combiner = [21970, 101067, 275230, 546362, 1513755, 2569199]
+
+y_hadoop_inmapper = [16067, 64016, 234068, 306486, 355855, 1923344]
+
+y_spark = [ , 140189, 335665, 615965, 1058833, 1702367]
+
+plt.plot(x_values, y_hadoop_combiner, marker='o', label='Hadoop with combiner function', color='blue')
+plt.plot(x_values, y_hadoop_inmapper, marker='*', label='Hadoop with in-mapper function', color='yellow')
+plt.plot(x_values, y_spark, marker='^', label='Spark function', color='red')
+
+plt.xlabel("Dataset size (MB)")
+plt.ylabel("Aggregate Resource Allocation (MB-seconds)")
+plt.title("resource comparison")
+plt.legend()
+plt.grid(True)
+
+# Mostra il grafico
+plt.tight_layout()
+plt.savefig("Hadoop-Spark (resources).png", dpi=300)  # dpi=300 per alta qualità
+plt.show()
--- invertedIndex.py
+++ invertedIndex.py
@@ -5,6 +5,36 @@ import nltk
 import os
 from nltk.corpus import stopwords
 
+# DATASET 2GB (file max 500KB)
+        # partition di 1000 files --> 15 min (stage 0) 2.3 min (stage 1) --> 18.3 min [300060_0058]
+        # partition di 500 files --> 14 min (stage 0) 3 min (stage 1) --> 17 min [300060_0059]
+        # partition di 250 files --> 13 min (stage 0) 4 min (stage 1) --> 17 min [300060_00]    QUESTO
+        # partition di 125 files -->  min (stage 0) min (stage 1) -->  min [300060_00]
+        # HADOOOOOOOOOP -> 13 minuti e 10 sec [0060_0062]
+        # NonParallel --> 32 minuti (19.8 files/s)
+
+        # DATASET 1GB (file max 500KB)
+        # partition di 1000 files --> 10 min (stage 0) 2 min (stage 1) --> 12 min [300060_0041]
+        # partition di 500 files --> 8.6 min (stage 0) 1.7 min (stage 1) --> 10.1 min [300060_0043]
+        # partition di 250 files --> 6.4 min (stage 0) 1.8 min (stage 1) --> 8.2 min [300060_0044]  QUESTO
+        # partition di 125 files --> 6.1 min (stage 0) 2.5 min (stage 1) --> 8.6 min [300060_0045]
+
+        # DATASET 300MB (file max 500KB)
+        # partition di 1000 files --> 3.4 min (stage 0) 23 s (stage 1) --> 4.4 min [300060_0048]
+        # partition di 500 files --> 3.1 min (stage 0)  33 s (stage 1) --> 3.7 min [300060_0049]
+        # partition di 250 files --> 2 min (stage 0) 30 s (stage 1) --> 2.5 min [300060_0050]   QUESTO
+        # partition di 125 files --> 1.9 min (stage 0) 34 s (stage 1) --> 2.4 min [300060_0051]
+
+        # DATASET 100MB (file max 500KB)
+        # partition di 1000 files --> 47 s (stage 0) 6 s (stage 1) --> 54 s [300060_0052] (2 task)
+        # partition di 500 files --> 1.1 min (stage 0)  8 s (stage 1) --> 1.2 min  [300060_0053] (3 task)
+        # partition di 250 files --> 37 s (stage 0) 9 s (stage 1) --> 47 s [300060_0055] (6 task)   QUESTO
+
+        # DATASET 246KB
+        # due partition da 3 files --> 6 s (stage 0) 1 s (stage 1) --> 7 s  [300060_0056] (2 task)      QUESTO
+
+# 2 : 10 = x : 60      x = 120 / 10
+
 # function that creates a contex of the application
 def create_spark_context(app_name="invertedIndex"):
     conf = SparkConf().setAppName(app_name)
@@ -52,7 +82,7 @@ if __name__ == "__main__":
     n_files = sum(1 for fileStatus in status if fileStatus.isFile())
 
     try:
-        rdd = sc.wholeTextFiles(input_dir).coalesce(max(1, int(n_files / 1000)))
+        rdd = sc.wholeTextFiles(input_dir, minPartitions=(max(2, int(n_files / 250))))
 
         # x[0] = fileName
         # x[1] = text
@@ -67,8 +97,8 @@ if __name__ == "__main__":
         rdd_words_filecount = rdd_count_words.map(lambda x: (x[0][0], f"{x[0][1]}:{x[1]}"))
 
         # [((il, (file1:2)), (il, (file3:4))] -> (il, (file1:2, file3:4))
-        rdd_final = rdd_words_filecount.groupByKey().mapValues(lambda vals: " ".join(vals))
-        # rdd_final = rdd_words_filecount.reduceByKey(lambda a, b: a + " " + b)
+        # rdd_final = rdd_words_filecount.groupByKey().mapValues(lambda vals: " ".join(vals))
+        rdd_final = rdd_words_filecount.reduceByKey(lambda a, b: a + " " + b)
 
         # save the result on the hdfs
         rdd_final.saveAsTextFile(output_dir)
